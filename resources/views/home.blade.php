d614bbb3b38ef0a6c43784bc1398d66e45965161
Andrea <b.a.benvenuti3@gmail.com>
2021-05-07 16:41:47 +0200

    aggiunte section del main

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -32,6 +32,41 @@
             </nav>
             <!-- /navbar -->
         </header>
+
+        <main>
+            <div class="container">
+                <section class="lunghe">
+                    <h2>LE LUNGHE</h2>
+                    <div class="cards">
+                        @foreach ($lunghe as $pasta)
+                        <div class="card m-10">
+                            <img src="{{$pasta['src']}}" alt="{{$pasta['titolo']}}">
+                        </div>
+                        @endforeach  
+                    </div>
+                </section>
+                <section class="lunghe">
+                    <h2>LE CORTE</h2>
+                    <div class="cards">
+                        @foreach ($corte as $pasta)
+                        <div class="card m-10">
+                            <img src="{{$pasta['src']}}" alt="{{$pasta['titolo']}}">
+                        </div>
+                        @endforeach  
+                    </div>
+                </section>
+                <section class="lunghe">
+                    <h2>LE CORTISSIME</h2>
+                    <div class="cards">
+                        @foreach ($cortissime as $pasta)
+                        <div class="card m-10">
+                            <img src="{{$pasta['src']}}" alt="{{$pasta['titolo']}}">
+                        </div>
+                        @endforeach  
+                    </div>
+                </section>
+            </div>
+        </main>
         
     </body>
 </html>
